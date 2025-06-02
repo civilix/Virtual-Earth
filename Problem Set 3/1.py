20ff00b5fc8c0fb9aa63ff92e4905fb5696c4c43
civilix <i@civilux.cc>
2025-06-02 11:59:01 +0900

    1

--- Problem Set 3/1.py
+++ Problem Set 3/1.py
@@ -2,21 +2,13 @@ import numpy as np
 import scipy.sparse as sp
 
 def DiffusionMatrix(n):
-    """
-    Constructs the diffusion matrix of size n x n with -2 on the diagonal
-    and 1 on the sub- and super-diagonals (sparse format).
-    """
     diagonals = [
-        -2 * np.ones(n),      # Main diagonal
-        np.ones(n - 1),       # Sub-diagonal
-        np.ones(n - 1)        # Super-diagonal
+        -2 * np.ones(n),
+        np.ones(n),
+        np.ones(n)
     ]
-    offsets = [0, -1, 1]      # Diagonal offsets: main (0), sub (-1), super (+1)
+    offsets = [0, -1, 1]
     A = sp.diags(diagonals, offsets, shape=(n, n), format='csr')
     return A
 
-# Example usage:
-n = 5
-A = DiffusionMatrix(n)
-print("Diffusion matrix (n=5):")
-print(A.toarray())
+print(DiffusionMatrix(5).toarray())
